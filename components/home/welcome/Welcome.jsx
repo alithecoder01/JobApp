58a818752dfb490729e7761681f6c2c566465e9f
Ali Hasan <capt.3lii@gmail.com>
2024-05-19 22:09:38 +0300

    added elements in welcome component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import { useState } from 'react' 
+import { useRouter } from 'expo-router'
 import styles from './welcome.style'
+import { icons,SIZES } from '../../../constants'
+import { 
+  View, 
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList 
+} from 'react-native'
+
+
+
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      {/* The name and the welocome text: */}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello, Ali</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      {/*  search bar: */}
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+          style={styles.searchInput}
+          />
+        </View>
+      </View>
+
     </View>
   )
 }
