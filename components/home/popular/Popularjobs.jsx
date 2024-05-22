2dbb247d12c1d3fa27f6a50bf2fd95162521d444
Ali Hasan <capt.3lii@gmail.com>
2024-05-23 01:35:27 +0300

    done from popular job card

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -12,11 +12,12 @@ import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 import useFetch from "../../../hook/useFetch";
 import styles from "./popularjobs.style";
 
+
 const Popularjobs = () => {
   const router = useRouter();
   const { data, isLoading, error } = useFetch("search", {
-    query: "react developer",
-    num_pages: 1,
+    query: "React developer",
+    num_pages: "1",
   });
   return (
     <View style={styles.container}>
@@ -30,14 +31,16 @@ const Popularjobs = () => {
       {/* For the content */}
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size="larg" color={COLORS.primary} />
+          <ActivityIndicator size='larg' color={COLORS.primary} />
         ) : error ? (
           <Text>Some thing went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
-            renderItem={({ item }) => <PopularJobCard item={item} />}
-            keyExtractor={(item) => item?.job_id}
+            data={data}
+            renderItem={({ item }) => (
+            <PopularJobCard 
+            item={item} />)}
+            keyExtractor={item => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
           />
